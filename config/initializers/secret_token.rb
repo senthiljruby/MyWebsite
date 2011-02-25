96daefcd8d0f0eb36869fa19e5b02f54aea98b58
senthiljruby <senthil.jruby@gmail.com>
2011-02-25 08:23:05 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Senthil::Application.config.secret_token = '4dbaf1ca631d6b2572a358085df6c19ad71451332cc408778af3ea4ce0baf6ba209d3616dd8c6084d41afda2b7119fe6cfd7434a0f37face11aabaa6360a3985'
